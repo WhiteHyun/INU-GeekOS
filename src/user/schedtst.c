ba04b1c17e84791d203c6671b3f8be17cf99804e
moonjupark <moonjupark@gmail.com>
2018-04-19 14:29:26 +0900

    for testing project 3

--- src/user/schedtst.c
+++ src/user/schedtst.c
@@ -23,29 +23,30 @@
 int main(int argc, char **argv) {
     int policy = -1;
     int holdsched3_sem;
-
     int id1, id2, id3;          /* ID of child process */
+    int quantum = 0;
+
     holdsched3_sem = Open_Semaphore("holdsched3_sem", 0);
 
-    if(argc >= 2) {
+    if(argc == 3) {
         if(!strcmp(argv[1], "rr")) {
             policy = 0;
         } else if(!strcmp(argv[1], "mys")) {
             policy = 1;
         } else {
-            Print("usage: %s [rr|mys]\n", argv[0]);
+            Print("usage: %s [rr|mys] <quantum>\n", argv[0]);
             Exit(1);
         }
-        Set_Scheduling_Policy(policy, 10);      /* second parameter may be unused. */
+        quantum = atoi(argv[2]);
+        Set_Scheduling_Policy(policy, quantum);
     } else {
-        Print("usage: %s [rr|mys]\n", argv[0]);
+        Print("usage: %s [rr|mys] <quantum>\n", argv[0]);
         Exit(1);
     }
 
-    id3 = Spawn_Program("/c/sched3.exe", "/c/sched3.exe", 0);
     id1 = Spawn_Program("/c/sched1.exe", "/c/sched1.exe", 0);
     id2 = Spawn_Program("/c/sched2.exe", "/c/sched2.exe", 0);
-
+    id3 = Spawn_Program("/c/sched3.exe", "/c/sched3.exe", 0);
 
     Wait(id1);
     Wait(id2);
