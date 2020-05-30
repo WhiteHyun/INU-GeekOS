df9a8a69b78edc1151a27647cd09bad9f4612224
WhiteHyun <cx_nf@naver.com>
2020-05-30 21:52:34 +0900

    Rename command

--- src/user/semtest1.c
+++ src/user/semtest1.c
@@ -16,31 +16,29 @@
 #include <sema.h>
 #include <string.h>
 
-#if !defined (NULL)
+#if !defined(NULL)
 #define NULL 0
 #endif
 
-int main(int argc __attribute__ ((unused)), char **argv
-         __attribute__ ((unused))) {
-    int scr_sem, holdp3_sem;    /* sid of screen semaphore */
-    int id1, id2, id3;          /* ID of child process */
+int main(int argc __attribute__((unused)), char **argv
+         __attribute__((unused)))
+{
+    int scr_sem, holdp3_sem; /* sid of screen semaphore */
+    int id1, id2, id3;       /* ID of child process */
 
     holdp3_sem = Open_Semaphore("holdp3_sem", 0);
     scr_sem = Open_Semaphore("screen", 1);
-
 
     P(scr_sem);
     Print("Semtest1 begins\n");
     V(scr_sem);
 
-
-    id3 = Spawn_Program("/c/p3.exe", "/c/p3.exe", 0);
+    id3 = Spawn_Program("/c/sem-p3.exe", "/c/sem-p3.exe", 0);
     P(scr_sem);
     Print("p3 created\n");
     V(scr_sem);
-    id1 = Spawn_Program("/c/p1.exe", "/c/p2.exe", 0);
-    id2 = Spawn_Program("/c/p2.exe", "/c/p1.exe", 0);
-
+    id1 = Spawn_Program("/c/sem-p1.exe", "/c/sem-p2.exe", 0);
+    id2 = Spawn_Program("/c/sem-p2.exe", "/c/sem-p1.exe", 0);
 
     Wait(id1);
     Wait(id2);
